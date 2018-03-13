194fc4d25fff6bf6e4fee6e3d2a7475e53737e30
Belle Jema Y. Bantilan <beljems@gmail.com>
2018-03-13 12:06:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>BEM Template</title>
+  </head>
+  <body>
+    <header class="header">
+      <div class="header__left">
+        <a href="" class="header__logo">...</a>
+      </div>
+      <div class="header__right">...</div>
+    </header>
+    <main class="main">
+      <div class="main__feature">...</div>
+    </main>
+    <div class="bottom">...</div>
+    <footer class="footer">
+      <div class="footer__left">...</div>
+      <div class="footer__right">...</div>
+    </footer>
+  </body>
+</html>
